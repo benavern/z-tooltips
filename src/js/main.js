be95aeacae209ac0d8053aa111a68843b235f103
benavern <bencrdc@gmail.com>
2015-12-01 00:15:30 +0100

    did a lot of things I didn't want to do .... tired!

--- src/js/main.js
+++ src/js/main.js
@@ -2,33 +2,85 @@
 
   // constructor
   this.ZTOOLTIPS = function(el) {
-    if(el == "undefined") throw Error("You must attach a DOM object!");
-    this.el = el;
-    this.text = this.el.dataset.ztooltips || "ZTooltips by ZeZeN.";
-
-    var _this = this;
-
-    __createTooltips();
-
-    this.el.addEventListener('mouseenter', function() {
-      _this.getText.call(_this);
-    })
-
-  }
-
+    if(el == "undefined") {
+      throw Error("You must attach a DOM object!");
+    }
+    else{
+      this.el = el;
+      this.text = this.el.dataset.ztooltips || "ZTooltips by ZeZeN.";
+      this.tooltip = null;
+      __createTooltips.call(this);
+    }
+  };
 
   // private functions
   function __createTooltips() {
-    var tooltip = document.createElement("div");
-    tooltip.className = "tooltips-container"
-    tooltip.innerHTML = this.text;
-    document.body.appendChild(tooltip);
+    // tooltip element creation
+    this.tooltip = document.createElement("div");
+    this.tooltip.className = "z-tooltip-container";
+    this.tooltip.innerHTML = this.text;
+    document.body.appendChild(this.tooltip);
+    //attach show/hide event triggers
+    __attachListeners.call(this);
   }
+
+  function __attachListeners() {
+    var _this = this;
+    // mouse enter
+    _this.el.addEventListener('mouseenter', function(e) {
+      _this.show.call(_this,e);
+    });
+    //mouse leave
+    _this.el.addEventListener('mouseleave', function() {
+      _this.hide.call(_this);
+    });
+  }
+
+  // fade in
+  function __fadeIn(el, display){
+    el.style.display = display || "block";
+    el.style.opacity = 0;
+    var setOpacity = function (){
+      el.style.opacity = parseFloat(el.style.opacity) + 0.1;
+      if(el.style.opacity < 1 || el.style.opacity < 1.0){
+        setTimeout(setOpacity, 100);
+      }
+    };
+    setOpacity();
+  }
+
+  // fade out
+  function __fadeOut(el){
+    var setOpacity = function (){
+      el.style.opacity = parseFloat(el.style.opacity) - 0.1;
+      if(el.style.opacity > 0 || el.style.opacity >0.0){
+        setTimeout(setOpacity, 100);
+      }
+      else{
+        el.style.display = "none";
+      }
+    };
+    setOpacity();
+  }
+
 
   // public functions
-  this.ZTOOLTIPS.prototype.getText = function(){
-    console.log(this.text);
-  }
+  this.ZTOOLTIPS.prototype.show = function(event){
+    var _this = this;
+
+    var position = {
+      x : event.pageX,
+      y : event.pageY
+    };
+    this.tooltip.style.left = position.x + "px";
+    this.tooltip.style.top  = position.y - + "px";
+
+    __fadeIn.call(this, this.tooltip);
+  };
+
+  this.ZTOOLTIPS.prototype.hide = function(){
+    __fadeOut.call(this, this.tooltip);
+  };
 
 
 
@@ -39,10 +91,12 @@
 
 
 
+//====================================================================
 
-
+//  Create all tooltips
 
   var ztooltips = document.querySelectorAll(".z-tooltips");
+
   for (var i=0; i<ztooltips.length; i++){
     (function(x) {
       new ZTOOLTIPS(ztooltips[x]);
